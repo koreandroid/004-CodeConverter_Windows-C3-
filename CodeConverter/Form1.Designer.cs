e40a4c5d5ce22be60c2c9690e7b0ba7272b356ec
koreandroid <dlwotjd116@snu.ac.kr>
2024-11-05 01:43:26 +0900

    design: Form1 기본 레이아웃 추가

--- CodeConverter/Form1.Designer.cs
+++ CodeConverter/Form1.Designer.cs
@@ -23,13 +23,73 @@
         /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.txtSourceCode = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtHistory = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtSourceCode
+            // 
+            this.txtSourceCode.CausesValidation = false;
+            this.txtSourceCode.Font = new System.Drawing.Font("GulimChe", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtSourceCode.Location = new System.Drawing.Point(64, 64);
+            this.txtSourceCode.Multiline = true;
+            this.txtSourceCode.Name = "txtSourceCode";
+            this.txtSourceCode.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtSourceCode.Size = new System.Drawing.Size(570, 633);
+            this.txtSourceCode.TabIndex = 0;
+            this.txtSourceCode.WordWrap = false;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnConvert.FlatAppearance.BorderSize = 0;
+            this.btnConvert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConvert.Font = new System.Drawing.Font("Gulim", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnConvert.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnConvert.Location = new System.Drawing.Point(650, 590);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(326, 75);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "변환하기";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            // 
+            // txtHistory
+            // 
+            this.txtHistory.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtHistory.Font = new System.Drawing.Font("Gulim", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtHistory.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.txtHistory.Location = new System.Drawing.Point(650, 366);
+            this.txtHistory.Multiline = true;
+            this.txtHistory.Name = "txtHistory";
+            this.txtHistory.ReadOnly = true;
+            this.txtHistory.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtHistory.Size = new System.Drawing.Size(326, 218);
+            this.txtHistory.TabIndex = 2;
+            this.txtHistory.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.txtHistory);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtSourceCode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CodeConverter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtSourceCode;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtHistory;
     }
 }
 
