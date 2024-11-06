5db65740fe817c2f861b18fb3d607ded04a17e00
koreandroid <dlwotjd116@snu.ac.kr>
2024-11-06 17:07:47 +0900

    add: Form1ViewModel.cs 파일

---
+++ CodeConverter/ViewModels/Form1ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeConverter.ViewModels
+{
+    internal sealed class Form1ViewModel {
+
+        internal string[] SourceCode { get; set; }
+
+        internal void ValidateSourceCode() {
+            ;
+        }
+    }
+}
